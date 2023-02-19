46a77b57718a2484f24c3584bf97cf722cc62535
Tresvalles14 <jhyar171@gmail.com>
2023-02-19 23:03:51 +0800

    Update cart

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -214,6 +214,7 @@ let re = /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,
                 </div>
                 <div className="nav__shop" id="cart-shop">
                     <i className='bx bx-shopping-bag'></i>
+                    <span className="cart-count">{myCart.length}</span>
                 </div>
                 <div className="nav__toggle" id="nav-toggle">
                     <i className="bx bx-grid-alt"></i>
